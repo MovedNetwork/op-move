3bdf460821a5a4dd8a96b7c94272227731618e79
code-dogg <realcodedogg@protonmail.com>
2025-06-08 18:48:05 +0200

    refactor(app): Add `ApplicationFactory` to encapsulate the creation behavior

--- app/src/factory.rs
+++ app/src/factory.rs
@@ -16,10 +16,59 @@ pub fn create<T: DependenciesThreadSafe>(
     (CommandQueue::new(tx, ktx), CommandActor::new(rx, app))
 }
 
+pub enum CreationMethod<D: DependenciesThreadSafe, F: ApplicationOnlyFactory<D>> {
+    Simultaneous(ApplicationReader<D>, Application<D>),
+    Deferred(ApplicationReader<D>, F),
+}
+
+pub trait ApplicationFactory<D: DependenciesThreadSafe, F: ApplicationOnlyFactory<D>> {
+    fn create(self) -> CreationMethod<D, F>;
+}
+
+pub trait ApplicationOnlyFactory<D: DependenciesThreadSafe> {
+    fn create(self) -> impl FnOnce() -> Application<D>;
+}
+
+impl<D: DependenciesThreadSafe> ApplicationOnlyFactory<D> for () {
+    fn create(self) -> impl FnOnce() -> Application<D> {
+        || unimplemented!("Unexpected call to create")
+    }
+}
+
+impl<D: DependenciesThreadSafe, F: FnOnce() -> (ApplicationReader<D>, Application<D>)>
+    ApplicationFactory<D, ()> for F
+{
+    fn create(self) -> CreationMethod<D, ()> {
+        let (app, reader) = self();
+
+        CreationMethod::Simultaneous(app, reader)
+    }
+}
+
+impl<D: DependenciesThreadSafe + Sized, F: FnOnce() -> Application<D>> ApplicationOnlyFactory<D>
+    for F
+{
+    fn create(self) -> impl FnOnce() -> Application<D> {
+        self
+    }
+}
+
+impl<
+    D: DependenciesThreadSafe,
+    F1: FnOnce() -> ApplicationReader<D>,
+    F2: FnOnce() -> Application<D>,
+> ApplicationFactory<D, F2> for (F1, F2)
+{
+    fn create(self) -> CreationMethod<D, F2> {
+        let reader = self.0();
+
+        CreationMethod::Deferred(reader, self.1)
+    }
+}
+
 /// Creates and runs the `future`.
-pub async fn run_deferred<D: DependenciesThreadSafe, F, Out>(
-    reader: impl FnOnce() -> ApplicationReader<D>,
-    app: impl FnOnce() -> Application<D>,
+pub async fn run_deferred<D: DependenciesThreadSafe, F, Out, T: ApplicationOnlyFactory<D>>(
+    factory: impl ApplicationFactory<D, T>,
     buffer: u32,
     future: impl FnOnce(CommandQueue, ApplicationReader<D>) -> F,
 ) -> Out
@@ -30,10 +79,20 @@ where
     let (ktx, _) = broadcast::channel(1);
     let (tx, rx) = mpsc::channel(buffer as usize);
     let queue = CommandQueue::new(tx, ktx);
-    let reader = reader();
-    let handle = future(queue, reader);
-    let mut app = app();
-    let actor = CommandActor::new(rx, &mut app);
 
-    crate::run(actor, handle)
+    match factory.create() {
+        CreationMethod::Simultaneous(reader, mut app) => {
+            let handle = future(queue, reader);
+            let actor = CommandActor::new(rx, &mut app);
+
+            crate::run(actor, handle).await
+        }
+        CreationMethod::Deferred(reader, factory) => {
+            let handle = future(queue, reader);
+            let mut app = factory.create()();
+            let actor = CommandActor::new(rx, &mut app);
+
+            crate::run(actor, handle).await
+        }
+    }
 }

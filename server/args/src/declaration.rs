ed67aac611d807be6e735a92741956550acb1060
code-dogg <realcodedogg@protonmail.com>
2025-06-15 12:46:30 +0200

    feat(server-args): Implement `Layer` that parses CLI arguments

--- server/args/src/declaration.rs
+++ server/args/src/declaration.rs
@@ -1,25 +1,35 @@
-use {optional_struct::Applicable, serde::Deserialize, std::net::SocketAddr};
+use {
+    clap::{Args, Parser},
+    optional_struct::Applicable,
+    serde::Deserialize,
+    std::net::SocketAddr,
+};
 
 #[optional_struct::optional_struct]
-#[derive(Deserialize, PartialEq, Debug, Clone)]
+#[derive(Deserialize, Parser, PartialEq, Debug, Clone)]
 pub struct Config {
     #[optional_rename(OptionalAuthSocket)]
     #[optional_wrap]
+    #[command(flatten)]
     pub auth: AuthSocket,
     #[optional_rename(OptionalHttpSocket)]
     #[optional_wrap]
+    #[command(flatten)]
     pub http: HttpSocket,
 }
 
 #[optional_struct::optional_struct]
-#[derive(Deserialize, PartialEq, Debug, Clone)]
+#[derive(Deserialize, Args, PartialEq, Debug, Clone)]
 pub struct HttpSocket {
+    #[arg(long = "http.addr", id = "http.addr")]
     pub addr: SocketAddr,
 }
 
 #[optional_struct::optional_struct]
-#[derive(Deserialize, PartialEq, Debug, Clone)]
+#[derive(Deserialize, Args, PartialEq, Debug, Clone)]
 pub struct AuthSocket {
+    #[arg(long = "auth.addr", id = "auth.addr")]
     pub addr: SocketAddr,
+    #[arg(long = "auth.jwt-secret", id = "auth.jwt-secret")]
     pub jwt_secret: String,
 }
---
+++ server/args/src/layers/cli.rs
@@ -0,0 +1,58 @@
+use {
+    crate::{declaration::OptionalConfig, stack::Layer},
+    clap::Parser,
+    std::{
+        env::{self, ArgsOs},
+        ffi::OsString,
+    },
+};
+
+#[derive(Debug, Clone, Default)]
+pub struct CliLayer<Args>(Args);
+
+impl CliLayer<ArgsOs> {
+    pub fn new() -> Self {
+        Self(env::args_os())
+    }
+}
+
+impl<Args: IntoIterator<Item: Into<OsString> + Clone>> Layer for CliLayer<Args> {
+    type Err = clap::Error;
+
+    fn try_load(self) -> Result<OptionalConfig, Self::Err> {
+        OptionalConfig::try_parse_from(self.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::declaration::{OptionalAuthSocket, OptionalHttpSocket},
+    };
+
+    #[test]
+    fn test_cli_layer_parses_arguments_successfully() {
+        let layer = CliLayer(vec![
+            "",
+            "--http.addr",
+            "0.0.0.0:1",
+            "--auth.addr",
+            "0.0.0.0:2",
+            "--auth.jwt-secret",
+            "test",
+        ]);
+        let actual_config = layer.try_load().unwrap();
+        let expected_config = OptionalConfig {
+            auth: Some(OptionalAuthSocket {
+                addr: "0.0.0.0:2".parse().ok(),
+                jwt_secret: Some("test".to_owned()),
+            }),
+            http: Some(OptionalHttpSocket {
+                addr: "0.0.0.0:1".parse().ok(),
+            }),
+        };
+
+        assert_eq!(actual_config, expected_config);
+    }
+}

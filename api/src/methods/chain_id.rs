d2c61433883bac575024db10f3d20c586b1b5cee
Ilia Churin <churin.ilya@gmail.com>
2025-07-12 00:33:01 +0900

    refactor(api): treat zero parameter queries uniformly

--- api/src/methods/chain_id.rs
+++ api/src/methods/chain_id.rs
@@ -1,12 +1,15 @@
 use {
-    crate::jsonrpc::JsonRpcError,
+    crate::{json_utils::parse_params_0, jsonrpc::JsonRpcError},
     umi_app::{ApplicationReader, Dependencies},
 };
 
 pub async fn execute<'reader>(
+    request: serde_json::Value,
     app: &ApplicationReader<'reader, impl Dependencies<'reader>>,
 ) -> Result<serde_json::Value, JsonRpcError> {
+    parse_params_0(request)?;
     let response = app.chain_id();
+
     Ok(serde_json::to_value(format!("{response:#x}"))
         .expect("Must be able to JSON-serialize response"))
 }
@@ -19,8 +22,15 @@ mod tests {
     async fn test_execute() {
         let (reader, _app) = create_app();
 
+        let request: serde_json::Value = serde_json::json!({
+            "jsonrpc": "2.0",
+            "method": "eth_chainId",
+            "params": [],
+            "id": 1
+        });
+
         let expected_response: serde_json::Value = serde_json::from_str(r#""0x194""#).unwrap();
-        let actual_response = execute(&reader).await.unwrap();
+        let actual_response = execute(request, &reader).await.unwrap();
 
         assert_eq!(actual_response, expected_response);
     }
--- api/src/methods/gas_price.rs
+++ api/src/methods/gas_price.rs
@@ -1,18 +1,39 @@
-use crate::jsonrpc::JsonRpcError;
+use umi_app::{ApplicationReader, Dependencies};
 
-pub async fn execute() -> Result<serde_json::Value, JsonRpcError> {
-    Ok(serde_json::to_value("0x3b9aca00").expect("Must be able to JSON-serialize response"))
+use crate::{json_utils::parse_params_0, jsonrpc::JsonRpcError};
+
+pub async fn execute<'reader>(
+    request: serde_json::Value,
+    app: &ApplicationReader<'reader, impl Dependencies<'reader>>,
+) -> Result<serde_json::Value, JsonRpcError> {
+    parse_params_0(request)?;
+
+    let response = app.gas_price()?;
+
+    Ok(serde_json::to_value(format!("{response:#x}"))
+        .expect("Must be able to JSON-serialize response"))
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::methods::tests::create_app;
+
     use super::*;
 
     #[tokio::test]
     async fn test_execute() {
-        // TODO: Return the actual gas price, currently hardcoded to 1,000,000,000
+        let (reader, _app) = create_app();
+
+        let request: serde_json::Value = serde_json::json!({
+            "jsonrpc": "2.0",
+            "method": "eth_gasPrice",
+            "params": [],
+            "id": 1
+        });
+
         let expected_response: serde_json::Value = serde_json::from_str(r#""0x3b9aca00""#).unwrap();
-        let response = execute().await.unwrap();
+        let response = execute(request, &reader).await.unwrap();
+
         assert_eq!(response, expected_response);
     }
 }
